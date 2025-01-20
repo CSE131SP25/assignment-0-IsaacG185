f05f616bbb1c7629e412dc3bd4d5f5e3847abdb1
isaacgutierrez <isaacgutierrez@localhost>
2025-01-20 16:55:46 -0600

    Assignment 0 completed

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,62 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(200);
+		robot.turnLeft(100);
+		robot.forward(100);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(30);
+		robot.turnRight(90);
+		robot.forward(100);
+		//repeat point
+		robot.turnLeft(130);
+		robot.forward(100);
+		robot.turnRight(90);
+		robot.forward(30);
+		robot.turnRight(90);
+		robot.forward(100);
+		//
+		robot.turnLeft(130);
+		robot.forward(100);
+		robot.turnRight(90);
+		robot.forward(30);
+		robot.turnRight(90);
+		robot.forward(100);
+		//
+		robot.turnLeft(130);
+		robot.forward(100);
+		robot.turnRight(90);
+		robot.forward(30);
+		robot.turnRight(90);
+		robot.forward(100);
+		//
+		robot.turnLeft(130);
+		robot.forward(100);
+		robot.turnRight(90);
+		robot.forward(30);
+		robot.turnRight(90);
+		robot.forward(100);
+		//
+		robot.turnLeft(130);
+		robot.forward(100);
+		robot.turnRight(90);
+		robot.forward(30);
+		robot.turnRight(90);
+		robot.forward(100);
+		//
+		robot.turnLeft(130);
+		robot.forward(100);
+		robot.turnRight(90);
+		robot.forward(30);
+		robot.turnRight(90);
+		robot.forward(100);
+		//
+		robot.turnLeft(199);
+		robot.forward(200);
+		robot.turnLeft(90);
+		robot.forward(55);
+		robot.turnLeft(91);
+		robot.forward(200);
 	}
 
 }
